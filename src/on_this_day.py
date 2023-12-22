2fda44f870b4cee9d3af7015102f1370dbaf79d4
Zachary Smith <22395357+Cutwell@users.noreply.github.com>
2023-12-22 02:04:29 +0000

    Update on_this_day.py

--- src/on_this_day.py
+++ src/on_this_day.py
@@ -18,14 +18,14 @@ def get_ordinal_suffix(day):
 
 
 def on_this_day(month: int, day: int):
-    """ Get a list of events that occurred on a given month / day.
+    """Get a list of events that occurred on a given month / day.
 
     Arguments:
         month (int): Target month (1-12).
         day (int): Target day (1-31).
     """
 
-    api_url = f'https://byabbe.se/on-this-day/{month}/{day}/events.json'
+    api_url = f"https://byabbe.se/on-this-day/{month}/{day}/events.json"
 
     try:
         response = requests.get(api_url)
@@ -36,10 +36,12 @@ def on_this_day(month: int, day: int):
 
         for e in data["events"]:
             events += f"{e['year']}: \t{e['description']}\n"
-        
-        date = datetime(year=2023, month=month, day=day).strftime(f"%d{get_ordinal_suffix(day)} of %B")
+
+        date = datetime(year=2023, month=month, day=day).strftime(
+            f"%d{get_ordinal_suffix(day)} of %B"
+        )
         print(TEMPLATE.format(date=date, events=events))
-        
+
         return data
 
     except requests.exceptions.RequestException as e:
@@ -47,13 +49,15 @@ def on_this_day(month: int, day: int):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Get the Wikipedia "On this day.." events.')
-    parser.add_argument('month', type=int, help='The target month (1-12)')
-    parser.add_argument('day', type=int, help='The target day (1-31)')
+    parser = argparse.ArgumentParser(
+        description='Get the Wikipedia "On this day.." events.'
+    )
+    parser.add_argument("month", type=int, help="The target month (1-12)")
+    parser.add_argument("day", type=int, help="The target day (1-31)")
 
     args = parser.parse_args()
     on_this_day(args.month, args.day)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
